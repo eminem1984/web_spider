8522724ae9b67affd4e034031f666e197a9374c1
eminem1984 <eminem_1984_@hotmail.com>
2016-11-03 15:29:13 +0800

    Delete qiubai_test.py

--- qiubai_test.py
+++
@@ -1,8 +0,0 @@
-import scrapy
-class main_spider(scrapy.spider):
-    name = "qiubai"
-    start_urls = [
-        "http://www.qiushibaike.com/",
-    ]
-    def parse(self, response):
-        print response.xpath('//div[@class="content"]').extract()
